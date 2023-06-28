e43932493ea7585ec9e200046ecdee41564826e1
Felipe Sander Dhein <113646192+NappS5@users.noreply.github.com>
2023-06-27 21:20:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -39,6 +39,47 @@ async function loadCharacters(url) {
             characterNameBG.appendChild(characterName)
             card.appendChild(characterNameBG)
 
+            card.onclick = () => {
+                const modal = document.getElementById("modal")
+                modal.style.visibility = "visible"
+
+                const modalContent = document.getElementById("modal-content")
+                modalContent.innerHTML = '';
+
+                const characterImage = document.createElement("div")
+                characterImage.style.backgroundImage = 
+                `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g, "")}.jpg')`
+                characterImage.className = "character-image"
+
+                const name = document.createElement("span")
+                name.className = "character-details"
+                name.innerText = `Nome: ${character.name}`
+
+                const characterHeight = document.createElement("span")
+                characterHeight.className = "character-details"
+                characterHeight.innerText = `Altura: ${convertHeight (character.height)}`
+
+                const mass = document.createElement("span")
+                mass.className = "character-details"
+                mass.innerText = `Peso: ${convertMass (character.mass)}`
+
+                const eyeColor = document.createElement("span")
+                eyeColor.className = "character-details"
+                eyeColor.innerText = `Cor dos olhos: ${convertEyeColor (character.eye_color)}`
+
+                const birthYear = document.createElement("span")
+                birthYear.className = "character-details"
+                birthYear.innerText = `Nascimento: ${covertBirthYear (character.birth_year)}`
+
+                modalContent.appendChild(characterImage)
+                modalContent.appendChild(name)
+                modalContent.appendChild(characterHeight)
+                modalContent.appendChild(mass)
+                modalContent.appendChild(eyeColor)
+                modalContent.appendChild(birthYear)
+
+            }
+
             mainContent.appendChild(card)
         });
 
@@ -87,4 +128,50 @@ async function loadPreviousPage() {
         console.log(error)
         alert('Erro ao carregar a página anterior')
     }
+}
+
+function hideModal() {
+    const modal = document.getElementById("modal")
+    modal.style.visibility = "hidden"
+}
+
+function convertEyeColor (eyeColor) {
+    const cores = {
+        blue: "azul",
+        brown: "castanho",
+        green: "verde",
+        yellow: "amarelo",
+        black: "preto",
+        pink: "rosa",
+        red: "vermelho",
+        orange: "laranja",
+        hazel: "avelã",
+        unknown: "desconhecida"
+    };
+
+    return cores [eyeColor.toLowerCase()] || eyeColor; //retorna cor dos olhos traduzida, se não der certo, retorna a normal.
+}
+
+function convertHeight(height) {
+    if (height === "unknown") {
+        return "desconhecida"
+    }
+
+    return (height / 100).toFixed(2);
+}
+
+function convertMass (mass) {
+    if (mass === "unknown") {
+        return "desconhecido"
+    }
+
+    return `${mass} kg`
+}
+
+function covertBirthYear(birthYear) {
+    if (birthYear === "unknown") {
+        return "desconhecido"
+    }
+
+    return birthYear
 }
